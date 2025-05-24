1f0ca3dc525a050e760d3f0fea8b1e55766551f8
NagaRoshini <nagaroshini7@gmail.com>
2025-05-24 17:46:12 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-function App() {
+const App = () => {
+  const slides = [
+    {
+      image: "MI.avif",
+      caption: "MI",
+    },
+    {
+      image: "CSK.webp",
+      caption: "CSK",
+    },
+    {
+      image: "RCBSquadWeb.jpg",
+      caption: "RCB",
+    },
+    {
+      image: "pbks.jpg",
+      caption: "PBKS",
+    },
+    {
+      image: "dc.avif",
+      caption: "DC",
+    },
+    {
+      image: "GT.webp",
+      caption: "GT",
+    },
+    {
+      image: "lsg.jpg",
+      caption: "LSG",
+    },
+    {
+      image: "RR-Live.webp",
+      caption: "RR",
+    },
+    {
+      image: "kkr.webp",
+      caption: "KKR",
+    },
+    {
+      image: "srh.webp",
+      caption: "SRH",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" style={{ width: "800px", margin: "auto", paddingTop: "50px" }}>
+        <h2>Image Slideshow</h2>
+        <Slider {...settings}>
+          {slides.map((slide, index) => (
+              <div key={index}>
+                <img
+                    src={slide.image}
+                    alt={slide.caption}
+                    style={{ width: "100%", height: "400px", objectFit: "cover" }}
+                />
+                <p style={{ textAlign: "center" }}>{slide.caption}</p>
+              </div>
+          ))}
+        </Slider>
+      </div>
   );
-}
+};
 
 export default App;
